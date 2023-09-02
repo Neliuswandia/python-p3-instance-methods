4276eb6e9835ba43329bb2a9bed5940050a79ccf
nelius <nelius.wandia@student.moringaschool.com>
2023-09-02 09:33:04 +0300

    completed assignment

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,16 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+# p = Person()
+# p.talk()
+
+
+    def walk(self):
+        print("The person is walking.")
+
+p = Person()
+p.talk()
+p.walk()      
